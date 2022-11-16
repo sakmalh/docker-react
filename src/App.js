a4e2d149cc106bada79c6fe9e24dc491a50e7cc7
Akmal Hameed <akmal.hameed@hiacuity.com>
2022-11-16 21:30:21 +0530

    travis

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Again Bilal!
+          Hello Akmal!
         </p>
         <a
           className="App-link"
