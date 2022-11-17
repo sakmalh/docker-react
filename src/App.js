753a7617d04ff8c629f5502cd1b046ed77205bc8
Akmal Hameed <akmal.hameed@hiacuity.com>
2022-11-17 10:25:14 +0530

    s

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Akmal!
+          Feature Branch
         </p>
         <a
           className="App-link"
